cf75d1e0066239caaaf0d54b4132af08ca24b30a
Qiao Hakusai <zbc0319@live.jp>
2021-05-18 16:47:05 +0900

    collection

--- HMS.py
+++ HMS.py
@@ -8,20 +8,23 @@ import pyautogui as mouse
 import math
 import win32gui, win32api, win32con
 
-
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 width, height = mouse.size()
 #cap = cv2.VideoCapture(0)
+
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  #设置宽度
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FPS, 30)
+fps = int(cap.get(5))
+print("fps:", fps)
 mpDraw = mp.solutions.drawing_utils
-mp_drawing = mp.solutions.drawing_utils
+#mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # 帧率统计
-pTime = 0
-cTime = 0
+# pTime = 0
+# cTime = 0
 
 # mouse.FAILSAFE = False
 x, y = mouse.position()
@@ -33,14 +36,14 @@ x = 0.0
 y = 0.0
 
 with mp_hands.Hands(max_num_hands=1,
-                        min_detection_confidence=0.8,
-                        min_tracking_confidence=0.8) as hands:
+                    min_detection_confidence=0.8,
+                    min_tracking_confidence=0.8) as hands:
     while True:
         success, img = cap.read()
         img = cv2.flip(img, 1)
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #转换为rgb
         results = hands.process(imgRGB)
-    
+
         #print(results.multi_hand_landmarks)
         if results.multi_hand_landmarks:
             start = time.time()
@@ -55,18 +58,24 @@ with mp_hands.Hands(max_num_hands=1,
                         'Visibility': data_point.visibility,
                     })
                 #print((keypoints[0:]["x"]))
-                mean_x = (mean([keypoints[0]["X"],keypoints[5]["X"],keypoints[17]["X"],keypoints[12]["X"]])) #选取几个手指的平均
-                mean_y = (mean([keypoints[0]["Y"],keypoints[5]["Y"],keypoints[17]["Y"],keypoints[12]["Y"]]))
+                mean_x = (mean([
+                    keypoints[0]["X"], keypoints[5]["X"], keypoints[17]["X"],
+                    keypoints[12]["X"]
+                ]))  #选取几个手指的平均
+                mean_y = (mean([
+                    keypoints[0]["Y"], keypoints[5]["Y"], keypoints[17]["Y"],
+                    keypoints[12]["Y"]
+                ]))
                 F_Z = keypoints[8]["Z"]
                 F_Y = keypoints[8]["Y"]
                 #keypoints[0]['X']
                 #print(mean_x,mean_y)
-                
+
                 #print((width) * (mean_x), (height) * (mean_y))
-                x = (width*2) * (mean_x) - width*0.5
-                y = (height*2) * (mean_y) - height*0.5
-                F_Z = (height*1.5) * (F_Z) - height*0.2
-                F_Y = (height*1.5) * (F_Y) - height*0.2
+                x = (width * 2) * (mean_x) - width * 0.5
+                y = (height * 2) * (mean_y) - height * 0.5
+                F_Z = (height * 1.5) * (F_Z) - height * 0.2
+                F_Y = (height * 1.5) * (F_Y) - height * 0.2
 
                 if x < 0:
                     x = 0
@@ -76,7 +85,7 @@ with mp_hands.Hands(max_num_hands=1,
                     x = width
                 if y > height:
                     y = height
-                
+
                 arr_F_Y[2] = arr_F_Y[1]
                 arr_F_Z[2] = arr_F_Z[1]
                 arr_F_Y[1] = arr_F_Y[0]
@@ -90,39 +99,40 @@ with mp_hands.Hands(max_num_hands=1,
                 arr_x[1] = arr_x[0]
                 arr_x[0] = x
                 arr_y[0] = y
-                
-                
-                avg_x = np.mean(arr_x)#增加稳定性
+
+                avg_x = np.mean(arr_x)  #增加稳定性
                 avg_y = np.mean(arr_y)
-                win32api.SetCursorPos((int(avg_x), int(avg_y)))#快的很
+                win32api.SetCursorPos((int(avg_x), int(avg_y)))  #快的很
 
                 # print("YY: ",arr_F_Y[2] - arr_F_Y[0])
                 # print("ZZ: ",arr_F_Z[2] - arr_F_Z[0])
 
                 #老的点击 （弱小
-                # if abs(arr_F_Y[2] - arr_F_Y[0])>160 and abs(arr_F_Z[2] - arr_F_Z[0])>25:
-                #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 200, 200, 0, 0)
-                #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 200, 200, 0, 0)
-                    #time.sleep(0.5)
-
+                if abs(arr_F_Y[2] - arr_F_Y[0]) > 160 and (arr_F_Z[2] -
+                                                           arr_F_Z[0]) < 25:
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 200,
+                                         200, 0, 0)
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 200, 200,
+                                         0, 0)
+                    #time.sleep(0.2)
 
                 #新的点击 （强大
-                index_finger_top = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                index_finger_second = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
+                # index_finger_top = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                # index_finger_second = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
 
-                distence = np.sqrt(np.sum(np.square(np.array([index_finger_top.x,index_finger_top.y])-np.array([index_finger_second.x,index_finger_second.y]))))
-                deep = index_finger_top.z - index_finger_second.z
-                
-                distence = distence*1980
-                print(distence)
-                if distence < 80 and abs(arr_F_Z[2] - arr_F_Z[0])>25:
-                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 200, 200, 0, 0)
+                # distence = np.sqrt(np.sum(np.square(np.array([index_finger_top.x,index_finger_top.y])-np.array([index_finger_second.x,index_finger_second.y]))))
+                # deep = index_finger_top.z - index_finger_second.z
 
-                if distence > 80 and abs(arr_F_Z[2] - arr_F_Z[0])>25:
-                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 200, 200, 0, 0)
+                # distence = distence*1980
+                # print(distence)
+                # if distence < 80 and abs(arr_F_Z[2] - arr_F_Z[0])>25:
+                #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 200, 200, 0, 0)
 
-                #mouse.moveTo(int(avg_x), int(avg_y),duration=0)#慢
-                #print(mouse.position())
+                # if distence > 80 and abs(arr_F_Z[2] - arr_F_Z[0])>25:
+                #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 200, 200, 0, 0)
+
+                # mouse.moveTo(int(avg_x), int(avg_y),duration=0)#慢
+                # print(mouse.position())
 
                 #判断手势
 
@@ -135,17 +145,16 @@ with mp_hands.Hands(max_num_hands=1,
                 #     cv2.putText(img, str(int(id)), (cx+10, cy+10), cv2.FONT_HERSHEY_PLAIN,
                 #                 1, (0, 0, 255), 2)
                 mpDraw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
-        
-        end = time.time()    
-        # 统计屏幕帧率
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
+
+        # end = time.time()
+        # # 统计屏幕帧率
+        # cTime = time.time()
+        # fps = 1 / (cTime - pTime)
+        # pTime = cTime
+        # cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        #             (255, 0, 255), 3)
 
         cv2.imshow("FMS", img)
         if cv2.waitKey(2) & 0xFF == 27:
             break
     cap.release()
-
